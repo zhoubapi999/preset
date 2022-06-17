0f6704248b5bcfd7448c7d36cf12b0888cb66ad8
zhoubapi999 <1508293898@qq.com>
2022-06-17 22:10:16 +0800

    ✨ 功能(deps): sys新增登录判断

--- src/sys.vue
+++ src/sys.vue
@@ -3,7 +3,7 @@
   <MescrollBody
     :up="{ use: enableUp }"
     :down="{ use: enableDown }"
-    @init="mescroll = $event"
+    @init="mescrollInit"
     @up="fetch"
     @down="enableUp ? $event.resetUpScroll() : fetch($event)"
   >
@@ -17,14 +17,19 @@ import MescrollBody from 'mescroll-uni/mescroll-body.vue'
 import { ScrollOptions } from '@/hooks'
 const { id } = $(useQuery())
 
-// 页面卸载,解绑回调事件
 onUnload(() => uni.$off(id + '_resolve'))
 onUnload(() => uni.$off(id + '_reject'))
 onUnload(() => uni.$off(id + '_query'))
 
 // mescroll 提供上拉加载下拉刷新能力
 // 文档: https://www.mescroll.com/uni.html
-const { mescroll, fetch, enable } = $(inject<Partial<ScrollOptions>>(ScrollSymbol, {}))
+let { mescroll, fetch, enable } = $(inject<Partial<ScrollOptions>>(ScrollSymbol, {}))
+
+let mescrollInit = async e => {
+  // 此处可做判断是否登录逻辑
+  mescroll = e
+}
+
 let enableUp = $computed(() => ['all', 'up'].some(e => e == enable))
 let enableDown = $computed(() => ['all', 'down'].some(e => e == enable))
 </script>
